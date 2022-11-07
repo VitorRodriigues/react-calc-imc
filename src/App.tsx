e328200268aee30b471bf680c0de821e510d9c9c
VitorRodriigues <tdvse@hotmail.com>
2022-11-07 20:04:57 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import style from './App.module.css';
+import poweredImage from './assets/powered.png';
+
+const App = () => {
+  return (
+    <div className={style.main}>
+      <header>
+        <div className={style.headerContainer}>
+          <img src={poweredImage} alt="" width={150}/>
+        </div>
+      </header>
+      <div className={style.container}>
+        <div className={style.leftSide}>
+          ...
+        </div>
+        <div className={style.rightSide}>
+          ...
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
